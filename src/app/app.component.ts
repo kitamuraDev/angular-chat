1007768ab49ff8f0dd93b4e87ec137e91b2f1832
kitamuraDev <masato1999824@icloud.com>
2023-05-07 23:08:53 +0900

    プロジェクトの初期設定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-chat';

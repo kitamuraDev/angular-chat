c372acb6c258995bef8fd83bcccc4c3465805c68
kitamuraDev <masato1999824@icloud.com>
2023-05-14 16:04:42 +0900

    :sparkles: part 73

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+
+// prettier-ignore
+const COMMENTS: Comment[] = [
+  { name: '武井 賢治', message: 'こんにちは', },
+  { name: '福永 治', message: 'どうも、こんにちは。いい天気ですね', },
+  { name: '市川 詩織', message: 'お散歩日和ですね！', },
+];
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
 }

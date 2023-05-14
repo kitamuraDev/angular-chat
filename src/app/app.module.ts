c5013542d2acad2576b8f39f9e7f0e6184b90fb8
kitamuraDev <masato1999824@icloud.com>
2023-05-14 23:37:00 +0900

    :sparkles: part 80
    /environments 直下の環境変数ファイルは apiキーなどが含まれているため gitの監視から除外

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { AppComponent } from './app.component';
 import { CommentDatePipe } from './pipes/comment-date.pipe';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, CommentDatePipe],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

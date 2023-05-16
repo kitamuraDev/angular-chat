29ca6c8abfedf472f5960d93d990e02fdd2a7cb6
kitamuraDev <masato1999824@icloud.com>
2023-05-16 23:06:46 +0900

    :sparkles: part 81

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 
 import { Comment } from './class/comment';
 import { User } from './class/user';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 const CURRENT_USER: User = new User(1, '五十川 洋平');
 const ANOTHER_USER: User = new User(2, '竹井 賢治');
@@ -23,6 +25,12 @@ export class AppComponent {
   comments = COMMENTS;
   currentUser = CURRENT_USER;
   chatMessage = '';
+
+  item$: Observable<any>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.item$ = db.object('/item').valueChanges(); // '/item' からデータを取得して、valueChanges() で Observable に変換する
+  }
 
   // 送信ボタンのクリックイベント
   addChatMessage(message: string): void {

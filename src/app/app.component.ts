77d9045cc438052448d639de74952eeec75716d1
kitamuraDev <masato1999824@icloud.com>
2023-05-17 23:18:32 +0900

    :sparkles: part 82

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Comment } from './class/comment';
 import { User } from './class/user';
 import { Observable } from 'rxjs';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 
 const CURRENT_USER: User = new User(1, '五十川 洋平');
 const ANOTHER_USER: User = new User(2, '竹井 賢治');
-
-// prettier-ignore
-const COMMENTS: Comment[] = [
-  new Comment(ANOTHER_USER, 'お疲れ様です！'),
-  new Comment(ANOTHER_USER, 'この間の件ですが、どうなりましたか？'),
-  new Comment(CURRENT_USER, 'お疲れ様です！'),
-  new Comment(CURRENT_USER, 'クライアントからOK出ました！'),
-];
 
 @Component({
   selector: 'ac-root',
@@ -22,18 +14,24 @@ const COMMENTS: Comment[] = [
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  comments = COMMENTS;
+  comments$: Observable<Comment[]>; // コメントデータ（データストア）
+  commentsRef: AngularFireList<Comment>; // realtimeDB を操作するためのインタフェース
   currentUser = CURRENT_USER;
-  chatMessage = '';
+  chatMessage = ''; // 入力されるinput
 
   item$: Observable<any>;
 
   constructor(private db: AngularFireDatabase) {
     this.item$ = db.object('/item').valueChanges(); // '/item' からデータを取得して、valueChanges() で Observable に変換する
+    this.commentsRef = db.list('/comments'); // DBからリスト参照（AngularFireList<T>）を返却
+    this.comments$ = this.commentsRef.valueChanges(); // Observable に変換して comments$ へ代入
   }
 
   // 送信ボタンのクリックイベント
   addChatMessage(message: string): void {
-    this.comments.push(new Comment(this.currentUser, message));
+    if (message) {
+      this.commentsRef.push(new Comment(this.currentUser, message));
+      this.chatMessage = ''; // 入力エリアのリセット
+    }
   }
 }

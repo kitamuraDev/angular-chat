03ff2f53b7e84530bbc64a00376345191d433079
kitamuraDev <masato1999824@icloud.com>
2023-05-21 22:56:06 +0900

    :sparkles: part 89

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,15 +6,17 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { environment } from '../../environments/environment';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
   ],
+  exports: [HeaderComponent],
 })
 
 // 一度だけ読み込む用のモジュール
